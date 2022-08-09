c04084673b91c16d57f7a7e8d2e73a0f708cdc1a
DillyzThe1 <dyl1029an@gmail.com>
2022-08-09 16:10:15 -0500

    some settings bc yes

--- source/shaders/ShimmerShader.hx
+++ source/shaders/ShimmerShader.hx
@@ -9,13 +9,15 @@ class ShimmerShader extends FlxShader
 
         uniform float totalElapsed;
         float shaderIntensity = 0.15;
+        float shaderSpeed = 1.15;
+        float shaderWidth = 3.75;
 
         void main() {
             vec4 color = flixel_texture2D(bitmap, openfl_TextureCoordv);
             vec4 outputPixel = vec4(color.x, color.y, color.z, 1.0);
 
             float uncleFredPNG = openfl_TextureCoordv.y + (openfl_TextureCoordv.x/2.5);
-            if (uncleFredPNG < mod(totalElapsed*1.15, 3.75) && uncleFredPNG > mod(totalElapsed*1.15, 3.75) - 0.25) {
+            if (uncleFredPNG < mod(totalElapsed*shaderSpeed, shaderWidth) && uncleFredPNG > mod(totalElapsed*shaderSpeed, shaderWidth) - 0.25) {
                 outputPixel.x += shaderIntensity;
                 outputPixel.y += shaderIntensity;
                 outputPixel.z += shaderIntensity;
